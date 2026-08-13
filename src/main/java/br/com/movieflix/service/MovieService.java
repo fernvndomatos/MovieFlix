a4e2998af25734760d5d8aec2c25bb1121888d20
Fernando Matos <fernando.matos221@gmail.com>
2026-08-13 19:40:46 -0300

    feat: Refatora metodo de salvar e cria endpoint para buscar por id

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,23 +1,51 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie){
+        movie.setCategories(findCategories(movie.getCategories()));
+        movie.setStreamings(findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id){
+        return movieRepository.findById(id);
+    }
+
+    private List <Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        for (Category category : categories) {
+            categoryService.findById(category.getId()).ifPresent(categoriesFound::add);
+        }
+        return categoriesFound;
+    }
+
+    private List <Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        for (Streaming streaming : streamings) {
+            streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add);
+        }
+        return streamingsFound;
     }
 }
