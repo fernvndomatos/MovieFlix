9cdab694f9cdddcad8144f7a75a23cd86faa70cb
Fernando Matos <fernando.matos221@gmail.com>
2026-08-14 15:44:45 -0300

    feat: Cria endpoint para listar filmes por categoria

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -45,4 +45,12 @@ public class MovieController {
                 .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam Long category){
+        return ResponseEntity.ok(movieService.findByCategory(category)
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList());
+    }
 }
--- src/main/java/br/com/movieflix/repository/MovieRepository.java
+++ src/main/java/br/com/movieflix/repository/MovieRepository.java
@@ -3,7 +3,10 @@ package br.com.movieflix.repository;
 import br.com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByCategoriesId(Long categoryId);
 }
