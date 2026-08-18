85d86df374763ae9fa674c42c9c694048e4dfef3
Fernando Matos <fernando.matos221@gmail.com>
2026-08-18 16:59:29 -0300

    feat: Cria Response, Request e Mapper de User

---
+++ src/main/java/br/com/movieflix/controller/Request/UserRequest.java
@@ -0,0 +1,4 @@
+package br.com.movieflix.controller.Request;
+
+public record UserRequest(String name, String email, String password) {
+}
---
+++ src/main/java/br/com/movieflix/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package br.com.movieflix.mapper;
+
+import br.com.movieflix.controller.Request.UserRequest;
+import br.com.movieflix.controller.Response.UserResponse;
+import br.com.movieflix.entity.User;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserMapper {
+
+    public static User toUser(UserRequest request){
+        return User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(request.password())
+                .build();
+
+    }
+
+    public static UserResponse toUserResponse(User user){
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+}
