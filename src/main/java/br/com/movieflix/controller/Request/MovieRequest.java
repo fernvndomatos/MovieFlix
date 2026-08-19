874c5df316585334e5a23fe7633649e309bebe2f
Fernando Matos <fernando.matos221@gmail.com>
2026-08-19 11:29:22 -0300

    Refactor: Refatora o metodo save melhorando tratamento de exceções

--- src/main/java/br/com/movieflix/controller/Request/MovieRequest.java
+++ src/main/java/br/com/movieflix/controller/Request/MovieRequest.java
@@ -1,13 +1,14 @@
 package br.com.movieflix.controller.Request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieRequest(String title,
+public record MovieRequest(@NotEmpty(message = "Titulo do filme é obrigatório.") String title,
                            String description,
                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                            LocalDate releaseDate,
